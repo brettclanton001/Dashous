f3e5979471ce82ffab2c319d7b66ab7df8ea30f9
Brett Clanton <brettclanton001@gmail.com>
2016-12-03 15:44:46 -0500

    improve devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
 
+  ## Public Pages
   root to: 'public#search'
-
   post '/', to: 'public#search'
   get '/t/:trade_request_id', to: 'public#trade_request', as: :public_trade_request
+  get '/p/:username', to: 'public#user_profile', as: :public_user_profile
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  ## Devise
+  devise_for :users, skip: [:sessions, :registrations]
+  as :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+    get 'signup', to: 'registrations#new', as: :new_user_registration
+    post 'signup', to: 'registrations#create', as: :user_registration
+  end
 
   ## User Restricted Area
   scope :u, module: :users do
@@ -19,5 +28,4 @@ Rails.application.routes.draw do
       end
     end
   end
-
 end
