7a07634700c355862da30f22730e33fff9530a35
Brett Clanton <brettclanton001@gmail.com>
2016-12-21 18:17:44 -0500

    improve the detect location feature

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,8 @@
 class PublicController < ApplicationController
   def search
-    @current_ip = request.remote_ip
+    if params[:detect_location].present?
+      @query = "#{request.location.city} #{request.location.country}"
+    end
     if @query
       @trade_requests = TradeRequest.near(@query, 5000).limit(20).decorate
     end
