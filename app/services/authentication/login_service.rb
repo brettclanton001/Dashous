edc3ce1e6ba68ba012a3d956f054bb0d77d4d95d
Brett Clanton <brettclanton001@protonmail.com>
2017-04-02 23:46:08 -0400

    compare passwords

--- app/services/authentication/login_service.rb
+++ app/services/authentication/login_service.rb
@@ -1,16 +1,27 @@
 class Authentication::LoginService
 
+  attr_reader :user, :password
+
   def initialize(params)
     @user = User.find_by_username params[:user][:username]
     @password = params[:user][:password]
   end
 
+  def authenticated?
+    return false unless user.present?
+    return false unless password.present?
+    return false unless password_correct?
+    true
+  end
+
   def create_session
   end
 
-  def authenticated?
-    return false unless @user.present?
-    return false unless @password.present?
-    true
+  private
+
+  def password_correct?
+    stored_hash = ::BCrypt::Password.new(user.encrypted_password)
+    provided_hash = ::BCrypt::Engine.hash_secret(password, stored_hash.salt)
+    stored_hash.to_s == provided_hash.to_s
   end
 end
